d6f6c3ef127ed175b20e05ad037d2322c79b48c6
Aung Thiha <mr.aungthiha@gmail.com>
2024-09-11 22:27:37 +0700

    feat: ktore class to fetch spacex launch list

---
+++ composeApp/src/commonMain/kotlin/aung/thiha/spacex/launches/data/network/SpaceXApi.kt
@@ -0,0 +1,24 @@
+package aung.thiha.spacex.launches.data.network
+
+import aung.thiha.spacex.launches.data.model.RocketLaunch
+import io.ktor.client.HttpClient
+import io.ktor.client.call.*
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.*
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+class SpaceXApi {
+    private val httpClient = HttpClient {
+        install(ContentNegotiation) {
+            json(Json {
+                ignoreUnknownKeys = true
+                useAlternativeNames = false
+            })
+        }
+    }
+
+    suspend fun getAllLaunches(): List<RocketLaunch> {
+        return httpClient.get("https://api.spacexdata.com/v5/launches").body()
+    }
+}
